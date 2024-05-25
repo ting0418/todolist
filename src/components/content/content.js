e98e898eee117dc8440d634ca5e946891446d117
ting <ting04180@gmail.com>
2024-05-26 01:06:13 +0800

    0526

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,6 +1,6 @@
 import style from "./content.module.scss";
 import React, { useState, useEffect, useRef } from "react";
-import Items from "../addItems/addItems";
+import Items from "../addItems/addItems.js";
 import { ProgressBar } from "react-bootstrap";
 const Content = () => {
   const [data, setData] = useState([
